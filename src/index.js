325be03efe8d060ee963a900f130b14481cb3013
Viktoryia Leshchyk <leshchik87@gmail.com>
2021-03-15 18:28:02 +0300

    feat: task solution complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let j = 0; j < bracketsConfig.length; j++) {
+        let arr = bracketsConfig[j];
+        let openhooks = arr[0];
+        let closehooks = arr[1];
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] == openhooks && str[i + 1] == closehooks) {
+                str = str.substr(0, i) + str.substr(i + 2);
+                i = -1;
+                j = -1;
+                continue;
+            }
+        }
+    }
+    return str.length === 0;
 }
